05a13b0252f59d8d07d94309ba0ddfa3f717717c
Oneshot369 <joshcpeck4@gmail.com>
2023-02-15 10:33:06 -0700

    I put a comment in to test if i can commit changes

--- topic3-2/src/main/java/com/josh/demo/Topic32Application.java
+++ topic3-2/src/main/java/com/josh/demo/Topic32Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.josh"})
 @SpringBootApplication
 public class Topic32Application {
-
+	//run this
 	public static void main(String[] args) {
 		SpringApplication.run(Topic32Application.class, args);
 	}

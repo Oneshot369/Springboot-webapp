9e5dedc2102184bc0c3a17204e5e5768ec33c732
Oneshot369 <joshcpeck4@gmail.com>
2023-03-30 18:50:49 -0700

    Added userEntity and updated usermodel to add id

--- topic3-2/src/main/java/com/josh/model/UserEntity.java
+++ topic3-2/src/main/java/com/josh/model/UserEntity.java
@@ -1,17 +1,30 @@
 package com.josh.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
-public class NewUserModel {
-
+@Table("users")
+public class UserEntity {
+    @Id
+    @Column("ID")
     Long Id;
+    
+    @Column("NAME")
     String Name;
+    @Column("LNAME")
     String LName;
+    @Column("EMAIL")
     String Email;
+    @Column("USERNAME")
     String Username;
+    @Column("PASSWORD")
     String Password;
     
-    
-    public NewUserModel(Long id, String name, String lName, String email, String username, String password) {
+    public UserEntity() {
+    }
+
+    public UserEntity(Long id, String name, String lName, String email, String username, String password) {
         Id = id;
         Name = name;
         LName = lName;
@@ -20,49 +33,59 @@ public class NewUserModel {
         Password = password;
     }
 
-    public NewUserModel() {
-        Id = null;
-        Name = null;
-        LName = null;
-        Email = null;
-        Username = null;
-        Password = null;
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
     }
 
     public String getName() {
         return Name;
     }
+
     public void setName(String name) {
         Name = name;
     }
+
     public String getLName() {
         return LName;
     }
+
     public void setLName(String lName) {
         LName = lName;
     }
+
     public String getEmail() {
         return Email;
     }
+
     public void setEmail(String email) {
         Email = email;
     }
+
     public String getUsername() {
         return Username;
     }
+
     public void setUsername(String username) {
         Username = username;
     }
+
     public String getPassword() {
         return Password;
     }
+
     public void setPassword(String password) {
         Password = password;
     }
 
     @Override
     public String toString() {
-        return "NewUserModel [Name=" + Name + ", LName=" + LName + ", Email=" + Email + ", Username=" + Username
-                + ", Password=" + Password + "]";
+        return "UserEntity [Id=" + Id + ", Name=" + Name + ", LName=" + LName + ", Email=" + Email + ", Username="
+                + Username + ", Password=" + Password + "]";
     }
+
+    
 }
